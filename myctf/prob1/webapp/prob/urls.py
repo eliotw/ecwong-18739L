b89fd2554e0a1a2e3e99c23353cc508f83fb3c01
Eliot <eliotw@cmu.edu>
2015-02-22 22:05:40 +0000

    Finish upload and parsing

--- myctf/prob1/webapp/prob/urls.py
+++ myctf/prob1/webapp/prob/urls.py
@@ -4,8 +4,9 @@ from prob import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', views.login_view, name='login'),
     url(r'^home/$', views.home, name='home'),
     url(r'^upload/$', views.upload, name='upload'),
+    url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^register/$', views.register, name='register')
 )
--- myctf/prob1/webapp/prob/views.py
+++ myctf/prob1/webapp/prob/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from prob.models import *
 from prob.forms import *
+from django.utils import timezone
+import re
+import time
+from cStringIO import StringIO
+import sys
 
 # Create your views here.
 
@@ -25,8 +31,11 @@ def index(request):
 
     return render(request, 'index.html', {'loginForm':loginForm, 'registerForm':u})
 
+def logout_view(request):
+    logout(request)
+    return redirect('/index/')
 
-def login(request):
+def login_view(request):
 
     u = LoginForm(request.POST)
 
@@ -35,26 +44,56 @@ def login(request):
         user = appUser.objects.filter(username=u.cleaned_data['username'], password=u.cleaned_data['password'])
 
         if len(user) > 0:
-                print "logged in"
+            user = authenticate(username='admin', password='password')
+            login(request, user)
+            return redirect('/index/home/')
         else:
             # TODO say login error
             return redirect('/index/')
+
     context = {}
+    register = RegisterForm()
+    context['registerForm'] = register
+    context['loginForm'] = u
 
-    return render(request, 'home.html', context)
+    return render(request, 'index.html', context)
 
+@login_required
 def home(request):
 
-    news = News.objects.all();
+    news = News.objects.all().order_by('-pub_date');
 
     context = {'news':news}
 
     return render(request, 'home.html', context)
 
+@login_required
 def upload(request):
-    
-    return render(request, 'upload.html', {})
 
+    if request.method == "GET":
+        return render(request, 'upload.html', {})
+
+    story = request.POST["story"]
+    m = re.findall("\[.*?\]", request.POST["story"])
+
+    for match in m:
+        try:
+            # Redirect output to get exec output
+            old_stdout = sys.stdout
+            redirected_output = sys.stdout = StringIO()
+            exec(match[1:-1])
+            sys.stdout = old_stdout
+            result = redirected_output.getvalue() 
+        except(NameError, SyntaxError) as e:
+            result="[command error]"
+            print e
+        story = story.replace(match, result)
+
+    admin = appUser.objects.get(username="admin")
+    s = News(user=admin, text=story, pub_date=timezone.now())
+    s.save()
+    
+    return redirect('/index/home/')
 def register(request):
 
     context = {}
